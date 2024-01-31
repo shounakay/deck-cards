b348a91a53fb7b97a32dd9ee6eef280ab99db563
Shounak Chavan <chavanshounak24@gmail.com>
2024-01-31 15:39:45 +0530

    chore: refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { TDeck, createDeck, generateRandomCards, test } from "./helpers";
+import { TDeck, createDeck, generateRandomCards } from "./helpers";
 import { DeckCard } from "./_components/DeckCard";
 import { DrawCard } from "./_components/DrawCard";
 
